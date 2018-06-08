eadc888a1016903a60bc3351c8f0ded38cfbcb5f
Gary Zeri <gzeri@model6.ps.uci.edu>
2018-06-07 18:05:40 -0700

    adds center of cartesian coordinates function to script

--- RMCM.py
+++ RMCM.py
@@ -17,7 +17,7 @@ instances = 2
 
 #areas where atoms can not be placed
 #[x list, y list, z list]
-#if coordinate exists in appropriate list, than that atom can not be spawned in that area
+#if coordinate exists in appropriate list, than that atom can not be placed in that area
 takenAreas = [[],[],[]]
 
 #######################################################
@@ -33,7 +33,28 @@ def parseSettings():
 
 #######################################################
 #get origin of specific structure
+#COCC = center of cartesian coordinates
+#is passed list of atoms in standard data structure
+def COCC(atoms):
 
+    #init empty list for center point
+    center = [ [0.0], [0.0], [0.0] ]    
 
+    #iterate through all atom data
+    for atom in atoms:
+	
+	#iterate through three dimensions
+	for dim in range(3):
+
+	    center[dim] += atom[dim+1]
+
+	
+	#get center point by dividing each dimension value by the number of atom in the structure
+	numberOfAtoms = len(atoms)
+        center[:] = [centerPoint / numberOfAtoms for centerPoint in center]
+
+    return center
+
+#######################################################
 
 
